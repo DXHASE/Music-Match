a6846a8a1e1e6fe2f42b4f3ad3dc031c93837ee9
DXHASE <danielchas886@gmail.com>
2022-08-29 19:28:57 -0500

    Console.logged input

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,9 +1,16 @@
 //Pull track then pull track id to put in the url of the variable lyrics 
-// Grab user input to find the track that is needed
+// Grab user input to find the track and the artist needed
+
 var inputForm = document.querySelector("#input-form");
 var userInput = document.querySelector(".input");
-var trackID ="";
-var getLyrics = function(){
-   console.log(inputForm);
+//var trackId ="";
+function getTrack(event){
+   event.preventDefault();
+   var trackUrl = "https://api.musixmatch.com/ws/1.1/track.lyric.get?" + trackId;
+   var input = userInput.value.trim()
+   .then(function(){
+
+   });
+   console.log(input)
 }
-inputForm.addEventListener("submit",getLyrics);
+inputForm.addEventListener("submit",getTrack);
